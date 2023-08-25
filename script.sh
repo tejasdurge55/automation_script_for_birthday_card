474b86b3466e3adbe6cb51433af27e345c6509b8
Tejas Yogesh Durge <107545687+tejasdurge55@users.noreply.github.com>
2023-08-25 22:13:51 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,14 +2,14 @@
 service apache2 start
 while true
 do
-  git clone https://github.com/tejasdurge55/birthday_gift_card.git /tejas2
-  cp -r /tejas2/birthday_wish_card/index.html /var/www/html/
-  cp -r /tejas2/birthday_wish_card/README.md /var/www/html/
-  cp -r /tejas2/birthday_wish_card/images /var/www/html/
-  cp -r /tejas2/birthday_wish_card/package.json /var/www/html/
-  cp -r /tejas2/birthday_wish_card/styles.css /var/www/html/
-  cp -r /tejas2/birthday_wish_card/vite.config.js /var/www/html/
-   sleep 10
+    git clone https://github.com/tejasdurge55/birthday_gift_card.git /tejas2
+    cp -r /tejas2/birthday_wish_card/index.html /var/www/html/
+    cp -r /tejas2/birthday_wish_card/README.md /var/www/html/
+    cp -r /tejas2/birthday_wish_card/images /var/www/html/
+    cp -r /tejas2/birthday_wish_card/package.json /var/www/html/
+    cp -r /tejas2/birthday_wish_card/styles.css /var/www/html/
+    cp -r /tejas2/birthday_wish_card/vite.config.js /var/www/html/
+    sleep 10
 done
 
 
