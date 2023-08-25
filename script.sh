1e697a33dfed34432a72874c9077f574bbc4b452
Tejas Yogesh Durge <107545687+tejasdurge55@users.noreply.github.com>
2023-08-25 21:56:58 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
-git clone https://github.com/tejasdurge55/birthday_gift_card.git /tejas/var/
-apt-get install -y tree
-
-tree /tejas/var/
-cp -r /tejas/var/birthday_wish_card/index.html /var/www/html/
-cp -r /tejas/var/birthday_wish_card/README.md /var/www/html/
-cp -r /tejas/var/birthday_wish_card/images /var/www/html/
-cp -r /tejas/var/birthday_wish_card/package.json /var/www/html/
-cp -r /tejas/var/birthday_wish_card/styles.css /var/www/html/
-cp -r /tejas/var/birthday_wish_card/vite.config.js /var/www/html/
+git clone https://github.com/tejasdurge55/birthday_gift_card.git /tejas2
+cp -r /tejas2/birthday_wish_card/index.html /var/www/html/
+cp -r /tejas2/birthday_wish_card/README.md /var/www/html/
+cp -r /tejas2/birthday_wish_card/images /var/www/html/
+cp -r /tejas2/birthday_wish_card/package.json /var/www/html/
+cp -r /tejas2/birthday_wish_card/styles.css /var/www/html/
+cp -r /tejas2/birthday_wish_card/vite.config.js /var/www/html/
 service apache2 start
