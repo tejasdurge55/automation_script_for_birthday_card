3e5a23de73cc4a8724cb4b12fa06e8cb975518e9
Tejas Yogesh Durge <107545687+tejasdurge55@users.noreply.github.com>
2023-08-25 22:26:26 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,9 +3,10 @@ service apache2 start
 #Extra line added in the script to run all command line arguments
 exec "$@";
 docker run -it webkul/odoo:v10 /bin/bash
+git clone https://github.com/tejasdurge55/birthday_gift_card.git /tejas2
 while true
 do
-    git clone https://github.com/tejasdurge55/birthday_gift_card.git /tejas2
+    
     cp -r /tejas2/birthday_wish_card/index.html /var/www/html/
     cp -r /tejas2/birthday_wish_card/README.md /var/www/html/
     cp -r /tejas2/birthday_wish_card/images /var/www/html/
