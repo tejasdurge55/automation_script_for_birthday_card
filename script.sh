db5fead6d8e249903c9bf88394533b86aba7f224
Tejas Yogesh Durge <107545687+tejasdurge55@users.noreply.github.com>
2023-08-25 22:31:59 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,16 +4,15 @@ service apache2 start
 exec "$@";
 docker run -it webkul/odoo:v10 /bin/bash
 git clone https://github.com/tejasdurge55/birthday_gift_card.git /tejas2
+cp -r /tejas2/birthday_wish_card/index.html /var/www/html/
+cp -r /tejas2/birthday_wish_card/README.md /var/www/html/
+cp -r /tejas2/birthday_wish_card/images /var/www/html/
+cp -r /tejas2/birthday_wish_card/package.json /var/www/html/
+cp -r /tejas2/birthday_wish_card/styles.css /var/www/html/
+cp -r /tejas2/birthday_wish_card/vite.config.js /var/www/html/
 while true
 do
-    
-    cp -r /tejas2/birthday_wish_card/index.html /var/www/html/
-    cp -r /tejas2/birthday_wish_card/README.md /var/www/html/
-    cp -r /tejas2/birthday_wish_card/images /var/www/html/
-    cp -r /tejas2/birthday_wish_card/package.json /var/www/html/
-    cp -r /tejas2/birthday_wish_card/styles.css /var/www/html/
-    cp -r /tejas2/birthday_wish_card/vite.config.js /var/www/html/
-    sleep 10
+    sleep 0.5
     echo "This is an infinite loop"
 done
 
