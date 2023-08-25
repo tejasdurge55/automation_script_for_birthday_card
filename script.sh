cde4942911c5481a2560c3680b9bf669a07c0b24
Tejas Yogesh Durge <107545687+tejasdurge55@users.noreply.github.com>
2023-08-25 20:39:53 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 git clone https://github.com/tejasdurge55/birthday_gift_card.git /tejas/var/
+apt-get install -y tree
+
+tree /tejas/var/
 cp -r /tejas/var/birthday_wish_card/index.html /var/www/html/
 cp -r /tejas/var/birthday_wish_card/README.md /var/www/html/
 cp -r /tejas/var/birthday_wish_card/images /var/www/html/
