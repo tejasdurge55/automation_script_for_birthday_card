e25da4f09378a542d5bb4de5fa3c4d531214ccb3
Tejas Yogesh Durge <107545687+tejasdurge55@users.noreply.github.com>
2023-08-25 20:12:49 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 git clone https://github.com/tejasdurge55/birthday_gift_card.git
-cp -r /automation_script_for_birthday_card/birthday_gift_card/birthday_wish_card/{index.html,README.md,images,package.json,styles.css,vite.config.js} /var/www/html/
+cp -r /birthday_gift_card/birthday_wish_card/{index.html,README.md,images,package.json,styles.css,vite.config.js} /var/www/html/
 service apache2 start
